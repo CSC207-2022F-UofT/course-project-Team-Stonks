a9cc21074e7afdae31a233be898d5c6574e8a740
Spaghetti-12 <elliot.sicheri@mail.utoronto.ca>
2022-10-28 10:28:09 -0400

    Fleshed out portfolio a bit

--- src/main/java/Portfolio.java
+++ src/main/java/Portfolio.java
@@ -1,7 +1,20 @@
 import java.util.Map;
 
 public class Portfolio {
-    public double balance;
+    private double balance;
     public String id;
     public Map<Stock, Integer> stocks;
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void changeBalance(double amount){
+        this.balance = this.balance + amount;
+    }
+
+    public void addStock(String symbol, int amount){
+        // Adds Stocks to the stock map
+
+    }
 }
