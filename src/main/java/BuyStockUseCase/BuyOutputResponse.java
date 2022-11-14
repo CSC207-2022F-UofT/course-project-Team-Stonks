f9cb0e81fb1a73fa789cb9915fa3dbc9745c0a77
Elliot Sicheri <113069552+Spaghetti-12@users.noreply.github.com>
2022-11-14 14:35:15 -0500

    Delete BuyOutputResponse.java

--- src/main/java/BuyStockUseCase/BuyOutputResponse.java
+++
@@ -1,9 +0,0 @@
-package BuyStockUseCase;
-
-public class BuyOutputResponse {
-    String output_text;
-
-    public BuyOutputResponse(String output_text){
-        this.output_text = output_text;
-    }
-}
