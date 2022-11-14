f2667c1bc74f99a0dcedf119fb309b359da29e86
Spaghetti-12 <elliot.sicheri@mail.utoronto.ca>
2022-11-14 14:46:37 -0500

    Undo accidental push

---
+++ src/main/java/BuyStockUseCase/BuyOutputResponse.java
@@ -0,0 +1,10 @@
+package BuyStockUseCase;
+
+public class BuyOutputResponse {
+    String output_text;
+
+    public BuyOutputResponse(String output_text){
+        this.output_text = output_text;
+    }
+}
+
---
+++ src/main/java/BuyStockUseCase/BuyUseCaseInteractor.java
@@ -0,0 +1,43 @@
+package BuyStockUseCase;
+
+import APIInterface.StockAPIAccess;
+import APIInterface.StockAPIRequest;
+import APIInterface.StockAPIResponse;
+import entities.Portfolio;
+import java.io.IOException;
+
+public class BuyUseCaseInteractor {
+
+    /**
+     * Receives symbol and quantity input from user, obtains stock price through
+     * StockAPIAccess, passes symbol, quantity and price to Portfolio
+     *
+     * @param req Request with user input
+     * @return BuyOutputResponse containing message to be displayed to user
+     * @throws IOException
+     */
+
+    BuyOutputResponse buyStock(BuyInputRequest req) throws IOException {
+        int buy_quantity = req.buy_quantity;
+        String symbol = req.symbol;
+        Portfolio port = req.port;
+
+        // Checks if user input is valid, prompts the user if not
+        if (buy_quantity < 1) {
+            return new BuyOutputResponse("Purchase failed. Please input a number greater than 0.");
+        }
+
+        // Accesses stock price from API
+        StockAPIAccess access = new StockAPIAccess();
+        StockAPIResponse res = access.accessAPI(new StockAPIRequest(symbol));
+        double price = res.price;
+
+        // Passes parameters to Portfolio, outputs results
+        boolean result = port.addStock(symbol, price, buy_quantity);
+        if (result) {
+            return new BuyOutputResponse("Purchase successful!");
+        } else {
+            return new BuyOutputResponse("Purchase failed. Insufficient balance.");
+        }
+    }
+}
