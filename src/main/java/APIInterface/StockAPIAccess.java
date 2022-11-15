865e14c7ff55fef22427da2c3b92570aadc44c49
Spaghetti-12 <elliot.sicheri@mail.utoronto.ca>
2022-11-15 17:18:43 -0500

    Everything should now be fixed (for real now)

--- src/main/java/APIInterface/StockAPIAccess.java
+++ src/main/java/APIInterface/StockAPIAccess.java
@@ -5,6 +5,7 @@ import yahoofinance.YahooFinance;
 import java.io.IOException;
 
 public class StockAPIAccess implements StockDatabaseAccess{
+    @Override
     public StockAPIResponse accessAPI(StockAPIRequest req) throws IOException {
         Stock stock = YahooFinance.get(req.getSymbol());
         return new StockAPIResponse(stock.getQuote().getPrice().doubleValue());
