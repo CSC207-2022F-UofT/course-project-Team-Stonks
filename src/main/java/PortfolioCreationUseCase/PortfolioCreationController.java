775e4ac5162f244e65018cfc2d70bf23ba996e48
jackzxliu <jacks.liu@mail.utoronto.ca>
2022-11-15 21:34:37 -0500

    changes to portfoliocreationcontroller: - added private interactor

--- src/main/java/PortfolioCreationUseCase/PortfolioCreationController.java
+++ src/main/java/PortfolioCreationUseCase/PortfolioCreationController.java
@@ -1,4 +1,9 @@
 package PortfolioCreationUseCase;
 
 public class PortfolioCreationController {
+    private PortfolioCreationInteractor pInteractor;
+
+    public PortfolioCreationController() {
+        pInteractor = new PortfolioCreationInteractor();
+    }
 }
