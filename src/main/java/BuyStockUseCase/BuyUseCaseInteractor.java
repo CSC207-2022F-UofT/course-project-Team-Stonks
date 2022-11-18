487009885e050486aba69026d8ee3d76bcadee41
eladperlman <eladperlman0@gmail.com>
2022-11-18 11:49:30 -0500

    Uncommented buy stock interactor code

--- src/main/java/BuyStockUseCase/BuyUseCaseInteractor.java
+++ src/main/java/BuyStockUseCase/BuyUseCaseInteractor.java
@@ -1,5 +1,10 @@
 package BuyStockUseCase;
 
+import APIInterface.StockAPIAccess;
+import APIInterface.StockAPIRequest;
+import APIInterface.StockAPIResponse;
+import entities.Portfolio;
+
 import java.io.IOException;
 
 public class BuyUseCaseInteractor {
@@ -13,27 +18,27 @@ public class BuyUseCaseInteractor {
      * @throws IOException
      */
 
-//    BuyOutputResponse buyStock(BuyInputRequest req) throws IOException {
-//        int buy_quantity = req.getQuantity();
-//        String symbol = req.getSymbol();
-//        Portfolio port = req.getPort();
-//
-//        // Checks if user input is valid, prompts the user if not
-//        if (buy_quantity < 1) {
-//            return new BuyOutputResponse(false);
-//        }
-//
-//        // Accesses stock price from API
-//        StockAPIAccess access = new StockAPIAccess();
-//        StockAPIResponse res = access.accessAPI(new StockAPIRequest(symbol));
-//        double price = res.getPrice();
-//
-//        // Passes parameters to Portfolio, outputs results
-//        boolean result = port.addStock(symbol, price, buy_quantity);
-//        if (result) {
-//            return new BuyOutputResponse(true);
-//        } else {
-//            return new BuyOutputResponse(false);
-//        }
-//    }
+    BuyOutputResponse buyStock(BuyInputRequest req) throws IOException {
+        int buy_quantity = req.getQuantity();
+        String symbol = req.getSymbol();
+        Portfolio port = req.getPort();
+
+        // Checks if user input is valid, prompts the user if not
+        if (buy_quantity < 1) {
+            return new BuyOutputResponse(false);
+        }
+
+        // Accesses stock price from API
+        StockAPIAccess access = new StockAPIAccess();
+        StockAPIResponse res = access.accessAPI(new StockAPIRequest(symbol));
+        double price = res.getPrice();
+
+        // Passes parameters to Portfolio, outputs results
+        boolean result = port.addStock(symbol, price, buy_quantity);
+        if (result) {
+            return new BuyOutputResponse(true);
+        } else {
+            return new BuyOutputResponse(false);
+        }
+    }
 }
