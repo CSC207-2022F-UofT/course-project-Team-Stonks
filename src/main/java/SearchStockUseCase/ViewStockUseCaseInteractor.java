3238793eebbee3dfa8af0318609702159378ac75
eladperlman <eladperlman0@gmail.com>
2022-11-28 00:21:47 -0500

    Changed lines affected by file renaming

--- src/main/java/SearchStockUseCase/ViewStockUseCaseInteractor.java
+++ src/main/java/SearchStockUseCase/ViewStockUseCaseInteractor.java
@@ -1,16 +1,13 @@
 package SearchStockUseCase;
-import APIInterface.StockAPIAccess;
+
+import APIInterface.StockAPIGateway;
 import APIInterface.StockAPIRequest;
 import APIInterface.StockAPIResponse;
-import yahoofinance.histquotes.Interval;
-
-import java.io.IOException;
-import java.util.Calendar;
 
 
 public class ViewStockUseCaseInteractor {
     public boolean searchStock(String symbol) throws Exception {
-        StockAPIResponse stockResponse = new StockAPIAccess().getPrice(new StockAPIRequest(symbol));
+        StockAPIResponse stockResponse = new StockAPIGateway().getPrice(new StockAPIRequest(symbol));
         ViewStockPresenter viewStockPresenter = new ViewStockPresenter(symbol);
         return true;
     }
