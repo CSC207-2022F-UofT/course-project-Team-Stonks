4143092a34f7bd969a31c0a71b729fcd5d8c884c
benmarlow <ben.marlow@mail.utoronto.ca>
2022-12-04 12:59:41 -0500

    Added test cases to Leaderboard Use Case.

--- src/test/java/LeaderboardTest/LeaderboardTest.java
+++ src/test/java/LeaderboardTest/LeaderboardTest.java
@@ -1,29 +1,23 @@
 package LeaderboardTest;
 
-import RegisterUseCase.RegisterController;
+import LeaderboardUseCase.LeaderboardResponse;
 import db.PortfolioDSRequest;
 import db.iEntityDBGateway;
+import entities.*;
 import main.OuterLayerFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import LeaderboardUseCase.LeaderboardUseCaseInteractor;
-import entities.UserManager;
-import entities.User;
 
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
 import java.sql.Date;
-import entities.Portfolio;
+
 import db.UserDSRequest;
-import entities.Leaderboard;
 
 public class LeaderboardTest {
     private static LeaderboardUseCaseInteractor interactor;
-    private static final String correctUsername = "database";
-    private static final String correctPassword = "password";
-    private static final String correctPasswordConfirm = "password";
     private static iEntityDBGateway dbGateway;
 
 
@@ -44,16 +38,60 @@ public class LeaderboardTest {
         PortfolioDSRequest portfolio2 = new PortfolioDSRequest("Portfolio2", 100000, "testUser2");
         dbGateway.addPortfolio(portfolio1);
         dbGateway.addPortfolio(portfolio2);
+        dbGateway.addCompPort("testUser1", "Portfolio1");
+        dbGateway.addCompPort("testUser2", "Portfolio2");
+
     }
 
+    /**
+     * Test that the getAllUsers method contains users
+     */
     @Test
     public void testGetAllUsersNonEmpty() {
         List<User> users = UserManager.instance.getAllUsers();
         Assertions.assertFalse(users.isEmpty());
     }
+
+    /**
+     * Test that the leaderboard displays the correct top users with the highest net values
+     */
     @Test
     public void testLeaderboardTwoUsers() {
-        Leaderboard newBoard = interactor.updateLeaderboard();
+        dbGateway.updatePortfolioBalance("Portfolio1", 100001, "testUser1");
+        dbGateway.updatePortfolioBalance("Portfolio2", 100000, "testUser2");
+        Leaderboard board = interactor.updateLeaderboard();
+        LeaderboardResponse response = new LeaderboardResponse(board);
+        List<String> stringList = response.toStringList();
+        assert (stringList.get(0).equals("1. testUser1:           $100001.0"));
+        assert (stringList.get(1).equals("2. testUser2:           $100000.0"));
+    }
 
+    /**
+     * Test that the leaderboard will update when the value of a top user's portfolio changes
+     */
+    @Test
+    public void testLeaderboardUpdate() {
+        dbGateway.updatePortfolioBalance("Portfolio2", 100002, "testUser2");
+        dbGateway.updatePortfolioBalance("Portfolio1", 100001, "testUser1");
+        Leaderboard board = interactor.updateLeaderboard();
+        LeaderboardResponse response = new LeaderboardResponse(board);
+        List<String> stringList = response.toStringList();
+        assert (stringList.get(0).equals("1. testUser2:           $100002.0"));
+        assert (stringList.get(1).equals("2. testUser1:           $100001.0"));
+    }
+
+    /**
+     * Test that the leaderboard will not change positions unless a user's portfolio exceeds the top user's
+     * in value, i.e. if they have the same amount, then the one that had that value first remains on top
+     */
+    @Test
+    public void testLeaderboardSameBal() {
+        dbGateway.updatePortfolioBalance("Portfolio1", 100003, "testUser1");
+        dbGateway.updatePortfolioBalance("Portfolio2", 100003, "testUser2");
+        Leaderboard board = interactor.updateLeaderboard();
+        LeaderboardResponse response = new LeaderboardResponse(board);
+        List<String> stringList = response.toStringList();
+        assert (stringList.get(0).equals("1. testUser1:           $100003.0"));
+        assert (stringList.get(1).equals("2. testUser2:           $100003.0"));
     }
 }
