9892239234d6636f9f8ecada01a5d0c1360a065b
noel-toms <noel.toms@mail.utoronto.ca>
2022-12-04 14:07:10 -0500

    Fixed GUI debugging message

--- src/main/java/LeaderboardUseCase/LeaderboardGUI.java
+++ src/main/java/LeaderboardUseCase/LeaderboardGUI.java
@@ -10,7 +10,10 @@ import java.awt.*;
 import java.util.List;
 import javax.swing.*;
 import javax.swing.border.TitledBorder;
+import javax.swing.plaf.FontUIResource;
+import javax.swing.text.StyleContext;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class LeaderboardGUI extends JFrame implements iLeaderboardGUI {
     private JPanel panel1;
@@ -54,7 +57,7 @@ public class LeaderboardGUI extends JFrame implements iLeaderboardGUI {
      */
     private void $$$setupUI$$$() {
         panel1 = new JPanel();
-        panel1.setLayout(new GridLayoutManager(3, 1, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.setLayout(new GridLayoutManager(3, 1, new Insets(40, 40, 40, 40), -1, -1));
         panel1.setBorder(BorderFactory.createTitledBorder(null, "", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, null));
         leaderBoard = new JList();
         final DefaultListModel defaultListModel1 = new DefaultListModel();
@@ -64,8 +67,32 @@ public class LeaderboardGUI extends JFrame implements iLeaderboardGUI {
         backButton.setText("Back");
         panel1.add(backButton, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JLabel label1 = new JLabel();
+        Font label1Font = this.$$$getFont$$$(null, Font.BOLD, 22, label1.getFont());
+        if (label1Font != null) label1.setFont(label1Font);
         label1.setText("Leaderboard");
         panel1.add(label1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    private Font $$$getFont$$$(String fontName, int style, int size, Font currentFont) {
+        if (currentFont == null) return null;
+        String resultName;
+        if (fontName == null) {
+            resultName = currentFont.getName();
+        } else {
+            Font testFont = new Font(fontName, Font.PLAIN, 10);
+            if (testFont.canDisplay('a') && testFont.canDisplay('1')) {
+                resultName = fontName;
+            } else {
+                resultName = currentFont.getName();
+            }
+        }
+        Font font = new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
+        boolean isMac = System.getProperty("os.name", "").toLowerCase(Locale.ENGLISH).startsWith("mac");
+        Font fontWithFallback = isMac ? new Font(font.getFamily(), font.getStyle(), font.getSize()) : new StyleContext().getFont(font.getFamily(), font.getStyle(), font.getSize());
+        return fontWithFallback instanceof FontUIResource ? fontWithFallback : new FontUIResource(fontWithFallback);
     }
 
     /**
@@ -74,4 +101,5 @@ public class LeaderboardGUI extends JFrame implements iLeaderboardGUI {
     public JComponent $$$getRootComponent$$$() {
         return panel1;
     }
+
 }
