2582417327de9e923eef64a673856d840800b7c8
ACER <hoathai.nguyen@mail.utoronto.ca>
2022-12-06 02:49:39 +0700

    Fix buy response for 0 stock

--- src/main/java/BuyStockUseCase/BuyStockPresenter.java
+++ src/main/java/BuyStockUseCase/BuyStockPresenter.java
@@ -37,7 +37,7 @@ public class BuyStockPresenter {
             return;
         }
 
-        if (quantity < 0) {
+        if (quantity <= 0) {
             view.displayInvalidInputFailure();
             return;
         }
